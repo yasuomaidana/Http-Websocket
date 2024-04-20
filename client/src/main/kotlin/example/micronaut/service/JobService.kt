8ea3831866352fc5739f74ca13486f1abd07dd30
yasuomaidana <yasuomaidana@gmail.com>
2024-04-20 16:47:33 +0900

    Adding websocket logic

--- client/src/main/kotlin/example/micronaut/service/JobService.kt
+++ client/src/main/kotlin/example/micronaut/service/JobService.kt
@@ -1,7 +1,23 @@
 package example.micronaut.service
 
+import io.micronaut.websocket.WebSocketSession
 import jakarta.inject.Singleton
 
 @Singleton
-class JobService {
+class JobService(){
+    // ... Job Management ...
+    private val webSocketSessions = mutableListOf<WebSocketSession>()
+
+    fun addWebSocketSession(session: WebSocketSession) {
+        webSocketSessions.add(session)
+    }
+
+    fun removeWebSocketSession(session: WebSocketSession){
+        webSocketSessions.remove(session)
+    }
+    // ... removeWebSocketSession ...
+
+    fun broadcastUpdates(jobUpdates: String) { // Assuming JSON string
+        webSocketSessions.forEach { it.sendSync(jobUpdates) }
+    }
 }
