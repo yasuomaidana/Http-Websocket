131c9a51363ed61cb1d4dc3028f016130e699f2b
yasuomaidana <yasuomaidana@gmail.com>
2024-04-22 18:48:17 +0900

    Configuring babel and react environment

---
+++ client/src/main/resources/frontend/job-view/webpack.config.js
@@ -0,0 +1,71 @@
+const { type } = require("os");
+const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const InterpolateHtmlPlugin = require('interpolate-html-plugin');
+
+const webpack = require('webpack');
+
+module.exports = (env) => {
+  const dotenvFilename  = env.NODE_ENV;
+  const envPath = path.resolve(__dirname, dotenvFilename);
+  const dotenv = require('dotenv')
+  const dotenvExpand = require('dotenv-expand')
+  const envVars = dotenvExpand.expand(dotenv.config({ path: envPath })).parsed || {};
+return {
+  entry: "./src/index.tsx", // Replace with your entry point
+  output: {
+    path: path.resolve(__dirname, envVars.BUILD_PATH), // Output directory for bundled files
+    publicPath: envVars.REACT_APP_STATIC_URL
+
+  },devServer: {
+    static: path.resolve(__dirname, envVars.BUILD_PATH),
+    port: 3000,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "babel-loader",
+        exclude: /node_modules/,
+      },{
+        test: /\.(ts|js)x?$/,
+        use: "babel-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          "style-loader", // Injects styles into DOM (development)
+          "css-loader", // Converts CSS to CommonJS modules
+          "sass-loader", // Compiles Sass to CSS
+        ],
+      },
+      { test: /\.txt$/, use: 'raw-loader' },
+      {
+        test: /\.(jpg|png|svg|jpeg)$/i,
+        type:"asset"
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js", ".jsx"],
+  },
+  optimization:{
+    minimize:true
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+      filename: envVars.BUILD_HTML+envVars.HTML_NAME+".html",
+    }),
+    new InterpolateHtmlPlugin({
+      REACT_APP_PUBLIC_URL: envVars.REACT_APP_PUBLIC_URL,
+      REACT_APP_STATIC_URL: envVars.REACT_APP_STATIC_URL,
+      PUBLIC_STATIC: envVars.PUBLIC_STATIC
+  }),
+    new webpack.DefinePlugin({
+      'process.env': JSON.stringify(envVars),
+    }),
+  ]
+
+}};
