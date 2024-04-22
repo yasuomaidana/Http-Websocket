6b1c174184cea99eaf55de6883166558c7e4c7eb
yasuomaidana <yasuomaidana@gmail.com>
2024-04-22 21:41:06 +0900

    Adding websocket and https as url and variables from environmental variables

---
+++ client/src/main/resources/frontend/job-view/src/url_config.ts
@@ -0,0 +1,14 @@
+const API_HTTP_URL = process.env.REACT_APP_API_HTTP
+const API_WEBSOCKET_URL = process.env.REACT_APP_API_WEBSOCKET
+
+export const Endpoints = {
+    websocket:{
+        base:`${API_WEBSOCKET_URL}`,
+        jobStatus:`${API_WEBSOCKET_URL}/jobs/status`
+    },
+    http:{
+        base:API_HTTP_URL,
+        jobs:`${API_HTTP_URL}/jobs`
+    }
+}
+
