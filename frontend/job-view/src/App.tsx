e1911c6d20653dd31dc2e0c0636b6ffcfc89188c
yasuomaidana <yasuomaidana@gmail.com>
2024-04-23 15:32:16 +0900

    Formatting document

--- frontend/job-view/src/App.tsx
+++ frontend/job-view/src/App.tsx
@@ -1,20 +1,21 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import './App.scss';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.scss";
 
-import React from 'react'
-import Temperature from './Temperature';
-import { PrintingView } from './PrintingView';
+import React from "react";
+import Temperature from "./Temperature";
+import { PrintingView } from "./PrintingView";
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter basename={process.env.PUBLIC_URL ?? process.env.REACT_APP_BASENAME}>
-            <Routes>
-              <Route path="" element={<PrintingView/>} />
-              {/* <Route path="job" element={<PrintingView/>} /> */}
-              <Route path="temperature" element={<Temperature/>}/>
-            </Routes>
-          </BrowserRouter>
+      <BrowserRouter
+        basename={process.env.PUBLIC_URL ?? process.env.REACT_APP_BASENAME}
+      >
+        <Routes>
+          <Route path="" element={<PrintingView />} />
+          <Route path="temperature" element={<Temperature />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
