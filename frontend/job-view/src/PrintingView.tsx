cb4c164973c898dfd040906e9be46c157005eb8a
yasuomaidana <yasuomaidana@gmail.com>
2024-04-27 17:08:03 +0900

    Splitting view

--- frontend/job-view/src/PrintingView.tsx
+++ frontend/job-view/src/PrintingView.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 import NewJobForm from "./NewJobForm";
 import PrintingJobs from "./PrintingJobs";
+import { Grid } from "@mui/material";
 
 
 export const PrintingView = () => {
@@ -10,15 +11,18 @@ export const PrintingView = () => {
       <header className="App-header">
         <h1>Jobs</h1>
       </header>
-      <div>
-        <NewJobForm />
-      </div>
-      <div>
-        <PrintingJobs />
-      </div>
+      <Grid container spacing={2}> {/* Wrap the components into a Material-UI grid */}
+        <Grid item xs={6}> {/* Make the left column occupy 50% of the screen */}
+          <PrintingJobs /> 
+        </Grid>
+        <Grid item xs={6}> {/* Make the right column occupy 50% of the screen */}
+          <NewJobForm /> 
+        </Grid>
+      </Grid>
       <div>Jobs Content</div>
-      <NavLink to="temperature"><button>Go to Temperature</button></NavLink>
-
+      <NavLink to="temperature">
+        <button>Go to Temperature</button>
+      </NavLink>
     </>
   );
 };
