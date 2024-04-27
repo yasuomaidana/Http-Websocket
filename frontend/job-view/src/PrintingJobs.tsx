3e970cf8d7c6519576e2e4fe905a9e44b475bcd3
yasuomaidana <yasuomaidana@gmail.com>
2024-04-27 16:44:47 +0900

    Showing all job features

--- frontend/job-view/src/PrintingJobs.tsx
+++ frontend/job-view/src/PrintingJobs.tsx
@@ -1,48 +1,47 @@
-import React, {useState, useEffect} from 'react'
-import { Endpoints } from './url_config';
+import React, { useState, useEffect } from "react";
+import { Endpoints } from "./url_config";
 
 interface Job {
-    id: number;
-    status: string;
-    remainingTime: number; 
-  }
-
-const PrintingJobs = () => {
-    const [jobs, setJobs] = useState<Job[]>([]);
-
-    useEffect(() => {
-      const websocket = new WebSocket(Endpoints.websocket.jobStatus);
-      let isConnected = false
-
-      websocket.onopen = () =>{
-        isConnected = true;
-      }
-      websocket.onmessage = (event) => {
-        const jobUpdates: Job[] = JSON.parse(event.data);
-        setJobs(jobUpdates);
-      };
-  
-      // Cleanup on unmount
-      return () => {
-        if (isConnected){
-            websocket.close();   
-        }
-    } 
-      
-    }, []); 
-  
-    return (
-      <div>
-        <h2>Printing Jobs List:</h2>
-        <ul id="job-list">
-          {jobs.map((job) => (
-            <li key={job.id}>
-              Job {job.id}: {job.status} - Remaining Time: {job.remainingTime}
-            </li>
-          ))}
-        </ul>
-      </div>
-    );
+  id: number;
+  name: string;
+  status: string;
+  totalTime: number; // Notice this field
 }
 
-export default PrintingJobs
+const PrintingJobs = () => {
+  const [jobs, setJobs] = useState<Job[]>([]);
+
+  useEffect(() => {
+    const websocket = new WebSocket(Endpoints.websocket.jobStatus);
+    let isConnected = false;
+
+    websocket.onopen = () => {
+      isConnected = true;
+    };
+    websocket.onmessage = (event) => {
+      const jobUpdates:Job[] = JSON.parse(event.data);
+      setJobs(jobUpdates);
+    }
+
+    // Cleanup on unmount
+    return () => {
+      if (isConnected) {
+        websocket.close();
+      }
+    };
+  }, []);
+  return (
+    <div>
+      <h2>Printing Jobs List:</h2>
+      <ul id="job-list">
+        {jobs.map((job,index) => (
+          <li key={index}>
+            Job {job.id}: {job.name} Satus: {job.status} - Remaining Time: {job.totalTime}
+          </li>
+        ))}
+      </ul>       
+    </div>
+  );
+};
+
+export default PrintingJobs;
