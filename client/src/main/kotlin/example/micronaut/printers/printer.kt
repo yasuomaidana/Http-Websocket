12ad845387cf92cbef53478d083cfacaa0f3fcf3
yasuomaidana <yasuomaidana@gmail.com>
2024-04-27 14:07:34 +0900

    Adding serializer to dataclasses

--- client/src/main/kotlin/example/micronaut/printers/printer.kt
+++ client/src/main/kotlin/example/micronaut/printers/printer.kt
@@ -1,5 +1,9 @@
 package example.micronaut.printers
 
+import kotlinx.serialization.Serializable
+
 data class Printer(val id: Int, var status: String = "free")
 data class PrintJobRequest(val name: String, val totalTime: Int)
+
+@Serializable
 data class PrintJob(val id: Int, val name: String, var status: String = "pending", var totalTime: Int)
