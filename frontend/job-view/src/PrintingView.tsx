b84714f0f4645a40646cfd52c33105339b833852
yasuomaidana <yasuomaidana@gmail.com>
2024-04-27 18:12:03 +0900

    Adding responsive view

--- frontend/job-view/src/PrintingView.tsx
+++ frontend/job-view/src/PrintingView.tsx
@@ -2,8 +2,7 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 import NewJobForm from "./NewJobForm";
 import PrintingJobs from "./PrintingJobs";
-import { Grid } from "@mui/material";
-
+import { Box, Button, Grid } from "@mui/material";
 
 export const PrintingView = () => {
   return (
@@ -11,18 +10,30 @@ export const PrintingView = () => {
       <header className="App-header">
         <h1>Jobs</h1>
       </header>
-      <Grid container spacing={2}> {/* Wrap the components into a Material-UI grid */}
-        <Grid item xs={6}> {/* Make the left column occupy 50% of the screen */}
-          <PrintingJobs /> 
+      <Grid container spacing={2}>
+        {/* Wrap the components into a Material-UI grid */}
+        <Grid item xs={12} sm={6} lg={9}>
+          {/* Make the left column occupy 50% of the screen */}
+          <PrintingJobs />
         </Grid>
-        <Grid item xs={6}> {/* Make the right column occupy 50% of the screen */}
-          <NewJobForm /> 
+        <Grid item xs={12} sm={6} lg={3}>
+          {/* Make the right column occupy 50% of the screen */}
+          <NewJobForm />
         </Grid>
       </Grid>
-      <div>Jobs Content</div>
-      <NavLink to="temperature">
-        <button>Go to Temperature</button>
-      </NavLink>
+
+      <Box display="flex" justifyContent="center" marginTop="2em">
+        <Grid container justifyContent="center" xs={12} sm={9} lg={4}>
+          <Button
+            variant="contained"
+            fullWidth
+            component={NavLink}
+            to="temperature"
+          >
+            Go to Temperature
+          </Button>
+        </Grid>
+      </Box>
     </>
   );
 };
