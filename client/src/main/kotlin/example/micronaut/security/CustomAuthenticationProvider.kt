6c8cb98ba9cf568b311a587be0028f03582a2d1e
yasuomaidana <yasuomaidana@gmail.com>
2024-05-08 06:52:43 +0900

    Adding inject to userRepo

--- client/src/main/kotlin/example/micronaut/security/CustomAuthenticationProvider.kt
+++ client/src/main/kotlin/example/micronaut/security/CustomAuthenticationProvider.kt
@@ -5,10 +5,12 @@ import io.micronaut.http.HttpRequest
 import io.micronaut.security.authentication.AuthenticationRequest
 import io.micronaut.security.authentication.AuthenticationResponse
 import io.micronaut.security.authentication.provider.HttpRequestAuthenticationProvider
+import jakarta.inject.Inject
 import jakarta.inject.Singleton
 
 @Singleton
 class CustomAuthenticationProvider<b>: HttpRequestAuthenticationProvider<HttpRequest<b>>{
+    @Inject
     lateinit var userRepo: UserRepository
 
     override fun authenticate(
