5196c11267b53e5bd207876b121c0f1e46fab6e4
yasuomaidana <yasuomaidana@gmail.com>
2024-05-22 17:12:32 +0900

    Implementing cookie based bearer token

--- client/src/main/kotlin/example/micronaut/controller/HomeController.kt
+++ client/src/main/kotlin/example/micronaut/controller/HomeController.kt
@@ -3,6 +3,7 @@ package example.micronaut.controller
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Get
 import io.micronaut.security.annotation.Secured
+import io.micronaut.security.authentication.Authentication
 import io.micronaut.security.rules.SecurityRule.IS_ANONYMOUS
 import io.micronaut.views.View
 
@@ -11,6 +12,25 @@ import io.micronaut.views.View
 class HomeController {
 
     @Get
+    @Secured(IS_ANONYMOUS)
     @View("home")
-    fun home(): Map<String, Any> = emptyMap()
+    fun home(authentication: Authentication?): Map<String, Any> {
+
+        var isAuthenticated = false
+        var isAdmin = false
+        if (authentication != null) {
+            isAuthenticated = true
+            isAdmin = authentication.roles.contains("admin")
+        }
+
+        return mapOf(
+            "isAuthenticated" to isAuthenticated,
+            "isAdmin" to isAdmin
+        )
+    }
+
+    @Get("/login")
+    @View("login")
+    fun login(): Map<String, Any> = emptyMap()
+
 }
--- client/src/main/kotlin/example/micronaut/controller/LoginController.kt
+++ client/src/main/kotlin/example/micronaut/controller/LoginController.kt
@@ -1,7 +1,4 @@
 package example.micronaut.controller
-
-import io.micronaut.http.HttpHeaders.AUTHORIZATION
-import io.micronaut.http.MediaType.TEXT_PLAIN
 import io.micronaut.http.annotation.*
 import io.micronaut.http.client.annotation.Client
 import io.micronaut.security.authentication.UsernamePasswordCredentials
@@ -12,7 +9,4 @@ interface  LoginController {
     @Post
     fun login(@Body credentials: UsernamePasswordCredentials): BearerAccessRefreshToken
 
-    @Consumes(TEXT_PLAIN)
-    @Get
-    fun home(@Header(AUTHORIZATION) authorization: String): String
 }
