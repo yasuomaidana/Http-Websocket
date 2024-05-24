3dc83096e5ed143a56b5607469769e5c99da98ff
yasuomaidana <yasuomaidana@gmail.com>
2024-05-24 22:55:27 +0900

    Implementing refresh token generation when remember is provided

---
+++ client/src/main/kotlin/example/micronaut/security/CustomRefreshTokenGenerator.kt
@@ -0,0 +1,21 @@
+package example.micronaut.security
+
+import io.micronaut.context.annotation.Replaces
+import io.micronaut.http.HttpRequest
+import io.micronaut.http.context.ServerRequestContext
+import io.micronaut.security.authentication.Authentication
+import io.micronaut.security.token.jwt.generator.RefreshTokenConfiguration
+import io.micronaut.security.token.jwt.generator.SignedRefreshTokenGenerator
+import jakarta.inject.Singleton
+import java.util.*
+
+@Singleton
+@Replaces(SignedRefreshTokenGenerator::class)
+class CustomRefreshTokenGenerator(config: RefreshTokenConfiguration?) : SignedRefreshTokenGenerator(config) {
+
+    override fun generate(authentication: Authentication?, token: String?): Optional<String> {
+        val serverRequest = ServerRequestContext.currentRequest<HttpRequest<*>>()
+        val remember = serverRequest.get().headers.get("remember")?.toBoolean() ?: false
+        return if (remember) super.generate(authentication, token) else Optional.empty()
+    }
+}
