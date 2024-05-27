c9b4a04d3adba61c43ca23595cd11e169532c84e
yasuomaidana <yasuomaidana@gmail.com>
2024-05-27 19:48:48 +0900

    Uploading an image

--- client/src/main/kotlin/example/micronaut/controller/upload/UploadController.kt
+++ client/src/main/kotlin/example/micronaut/controller/upload/UploadController.kt
@@ -2,6 +2,7 @@ package example.micronaut.controller.upload
 
 import example.micronaut.entities.Image
 import example.micronaut.repository.ImageRepository
+import io.micronaut.http.HttpResponse
 import io.micronaut.http.MediaType.*
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Part
@@ -10,7 +11,10 @@ import io.micronaut.http.multipart.CompletedFileUpload
 import io.micronaut.security.annotation.Secured
 import io.micronaut.security.rules.SecurityRule.IS_ANONYMOUS
 import java.io.ByteArrayInputStream
+import java.io.IOException
 import java.net.URLConnection
+import java.nio.file.Files
+import java.nio.file.Paths
 
 @Controller("/upload")
 @Secured(IS_ANONYMOUS)
@@ -32,6 +36,19 @@ class UploadController (
         }
         imageRepository.save(image)
         return "Image uploaded successfully"
+    }
+
+    @Post("/image", consumes = [MULTIPART_FORM_DATA])
+    fun uploadImage(@Part file: CompletedFileUpload): HttpResponse<String> {
+        try {
+            val fileName = file.filename
+            val filePath = Paths.get("uploads/$fileName")
+            Files.createDirectories(filePath.parent)
+            Files.write(filePath, file.bytes)
+            return HttpResponse.ok("Image uploaded successfully")
+        } catch (e: IOException) {
+            return HttpResponse.badRequest("Failed to upload image")
+        }
     }
 }
 
