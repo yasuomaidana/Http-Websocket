df341df962732ce23b6cd6440daf3c9253b07ca2
yasuomaidana <yasuomaidana@gmail.com>
2024-05-28 13:50:23 +0900

    Deleting users

--- client/src/main/kotlin/example/micronaut/controller/RegisterController.kt
+++ client/src/main/kotlin/example/micronaut/controller/RegisterController.kt
@@ -12,7 +12,9 @@ import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Get
 import io.micronaut.http.annotation.Post
 import io.micronaut.security.annotation.Secured
+import io.micronaut.security.authentication.Authentication
 import io.micronaut.security.rules.SecurityRule.IS_ANONYMOUS
+import io.micronaut.security.rules.SecurityRule.IS_AUTHENTICATED
 import io.micronaut.views.View
 import jakarta.annotation.security.RolesAllowed
 import jakarta.inject.Inject
@@ -52,5 +54,12 @@ class RegisterController {
     fun registerUserWithRoles(@Body registerUserRequest: RegisterUserRequest): HttpResponse<String> {
         userService.registerUserWithRoles(registerUserRequest)
         return HttpResponse.ok("User with roles registered successfully.")
+    }
+
+    @Post("/delete")
+    @Secured(IS_AUTHENTICATED)
+    fun deleteUser(authentication:Authentication): HttpResponse<String> {
+        userService.deleteUserByUsername(authentication.name)
+        return HttpResponse.ok("${authentication.name} deleted successfully.")
     }
 }
