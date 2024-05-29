982d2317073235be96750b1b7de794149135444a
yasuomaidana <yasuomaidana@gmail.com>
2024-05-29 22:29:41 +0900

    Adding maximum age

--- client/src/main/kotlin/example/micronaut/configuration/refreshtoken/CustomRefreshTokenConfiguration.kt
+++ client/src/main/kotlin/example/micronaut/configuration/refreshtoken/CustomRefreshTokenConfiguration.kt
@@ -5,9 +5,15 @@ import kotlin.time.Duration
 interface CustomRefreshTokenConfiguration: RefreshTokenConfiguration {
 
     var expirationTime: Duration
+    var maximumAge: Duration?
 
     fun setExpirationTime(expirationTime: String) {
         this.expirationTime = Duration.parse(expirationTime)
+        this.maximumAge = this.maximumAge ?: this.expirationTime
+    }
+
+    fun setMaximumAge(maximumAge: String) {
+        this.maximumAge = Duration.parse(maximumAge)
     }
 
 }
