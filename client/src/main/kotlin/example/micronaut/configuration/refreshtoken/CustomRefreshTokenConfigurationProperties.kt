0cfc48b03ec3e4aa2bc0e9ceadb2ffc7dd362265
yasuomaidana <yasuomaidana@gmail.com>
2024-05-29 20:24:26 +0900

    Removing setters

--- client/src/main/kotlin/example/micronaut/configuration/refreshtoken/CustomRefreshTokenConfigurationProperties.kt
+++ client/src/main/kotlin/example/micronaut/configuration/refreshtoken/CustomRefreshTokenConfigurationProperties.kt
@@ -5,53 +5,33 @@ import com.nimbusds.jose.JWSAlgorithm
 import io.micronaut.context.annotation.ConfigurationProperties
 import io.micronaut.context.annotation.Replaces
 import io.micronaut.context.annotation.Requires
-import io.micronaut.core.annotation.NonNull
+import io.micronaut.core.annotation.Introspected
 import io.micronaut.core.util.StringUtils
 import io.micronaut.security.token.jwt.generator.RefreshTokenConfiguration
-
 import io.micronaut.security.token.jwt.generator.RefreshTokenConfigurationProperties
 import io.micronaut.security.token.jwt.generator.RefreshTokenConfigurationProperties.*
-import jakarta.validation.constraints.NotBlank
-import jakarta.validation.constraints.NotNull
 import kotlin.time.Duration
 
 interface CustomRefreshTokenConfigurationInterface: RefreshTokenConfiguration {
     fun setExpirationTime(expirationTime: String)
 }
 
+@Introspected
 @Replaces(RefreshTokenConfigurationProperties::class)
 @Requires(property = "$PREFIX.secret")
 @Requires(property = "$PREFIX.enabled", notEquals = StringUtils.FALSE)
 @ConfigurationProperties(PREFIX)
-class CustomRefreshTokenConfigurationProperties :CustomRefreshTokenConfigurationInterface {
+class CustomRefreshTokenConfigurationProperties: CustomRefreshTokenConfigurationInterface {
 
     var expirationTime: Duration = Duration.ZERO
-    private var enabled = DEFAULT_ENABLED
-
-    @NonNull
-    @NotBlank
-    private var jwsAlgorithm: JWSAlgorithm = DEFAULT_JWS_ALGORITHM
-
-    @NonNull
-    @NotBlank
-    private var secret: String = ""
-    private var base64 = DEFAULT_BASE64
-
-    fun setEnabled(enabled: Boolean) {
-        this.enabled = enabled
-    }
-
-    fun setJwsAlgorithm(jwsAlgorithm: JWSAlgorithm) {
-        this.jwsAlgorithm = jwsAlgorithm
-    }
-
-    fun setSecret(secret: String) {
-        this.secret = secret
-    }
-
-    fun setBase64(base64: Boolean) {
-        this.base64 = base64
-    }
+    @JvmField
+    var enabled: Boolean = DEFAULT_ENABLED
+    @JvmField
+    var  jwsAlgorithm: JWSAlgorithm = DEFAULT_JWS_ALGORITHM
+    @JvmField
+    var secret: String = ""
+    @JvmField
+    var base64: Boolean = DEFAULT_BASE64
 
     override fun setExpirationTime(expirationTime: String) {
         this.expirationTime = Duration.parse(expirationTime)
@@ -61,7 +41,6 @@ class CustomRefreshTokenConfigurationProperties :CustomRefreshTokenConfiguration
         return jwsAlgorithm
     }
 
-    @NotNull
     override fun getSecret(): String {
         return secret
     }
@@ -69,5 +48,4 @@ class CustomRefreshTokenConfigurationProperties :CustomRefreshTokenConfiguration
     override fun isBase64(): Boolean {
         return base64
     }
-
 }
