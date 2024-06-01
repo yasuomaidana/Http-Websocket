e5c3d0b0c9fa59d27f1e1c9c13208ae9f4ccce4d
yasuomaidana <yasuomaidana@gmail.com>
2024-06-01 18:38:08 +0900

    Adding find by revoked and expires on

--- client/src/main/kotlin/example/micronaut/repository/RefreshTokenRepository.kt
+++ client/src/main/kotlin/example/micronaut/repository/RefreshTokenRepository.kt
@@ -5,22 +5,17 @@ import io.micronaut.data.jdbc.annotation.JdbcRepository
 import io.micronaut.data.model.query.builder.sql.Dialect.MYSQL
 import io.micronaut.data.repository.CrudRepository
 import java.util.Optional
-import jakarta.transaction.Transactional
 import jakarta.validation.constraints.NotBlank
+import java.time.Instant
 
 @JdbcRepository(dialect= MYSQL)
 interface RefreshTokenRepository : CrudRepository<RefreshToken, Long> {
 
-    @Transactional
-    fun save(@NotBlank username: String,
-             @NotBlank refreshToken: String,
-             revoked: Boolean): RefreshToken
-
     fun findByRefreshToken(@NotBlank refreshToken: String): Optional<RefreshToken>
 
-    fun updateByUsername(@NotBlank username: String,
-                         revoked: Boolean): Long
-    fun deleteByUsername(@NotBlank username: String): Long
-
      fun findByUsername(username: String): List<RefreshToken>
+
+     fun findByRevoked(revoked: Boolean): List<RefreshToken>
+
+     fun findByExpiresOnBefore(expiresOn: Instant): List<RefreshToken>
 }
