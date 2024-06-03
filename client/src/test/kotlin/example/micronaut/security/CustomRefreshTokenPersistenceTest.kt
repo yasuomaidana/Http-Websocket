d51384900de772c389028b8768639e67a1a11b85
yasuomaidana <yasuomaidana@gmail.com>
2024-06-03 13:17:29 +0900

    Implementing tests

--- client/src/test/kotlin/example/micronaut/security/CustomRefreshTokenPersistenceTest.kt
+++ client/src/test/kotlin/example/micronaut/security/CustomRefreshTokenPersistenceTest.kt
@@ -4,11 +4,12 @@ import example.micronaut.configuration.refreshtoken.CustomRefreshTokenConfigurat
 import example.micronaut.entities.RefreshToken
 import example.micronaut.repository.RefreshTokenRepository
 import io.micronaut.security.authentication.Authentication
+import io.micronaut.security.errors.OauthErrorResponseException
 import io.micronaut.security.token.event.RefreshTokenGeneratedEvent
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
-
+import io.micronaut.security.errors.IssuingAnAccessTokenErrorCode.INVALID_GRANT
 import org.junit.jupiter.api.Assertions.*
 import org.mockito.ArgumentMatchers.any
 import org.mockito.Mock
@@ -62,6 +63,44 @@ class CustomRefreshTokenPersistenceTest {
 //        Assertions.assertNull(authentication)
 //        Mockito.verify(refreshTokenRepository, times(1)).update(refreshToken)
 //    }
+@Test
+fun testGetAuthenticationExpiredToken() {
+    val refreshToken = RefreshToken(
+        username = "testUser",
+        refreshToken = "testRefreshToken",
+        expiresOn = Instant.now().minusSeconds(60)
+    )
+    Mockito.`when`(refreshTokenRepository.findByRefreshToken("testRefreshToken")).thenReturn(Optional.of(refreshToken))
+
+
+    var authentication: Authentication? = null
+    var error: Throwable? = null
+    customRefreshTokenPersistence.getAuthentication("testRefreshToken").subscribe(object : Subscriber<Authentication> {
+        override fun onSubscribe(s: Subscription) {
+            s.request(1)
+        }
+
+        override fun onNext(t: Authentication) {
+            authentication = t
+        }
+
+        override fun onError(t: Throwable) {
+            error = t
+        }
+
+        override fun onComplete() {
+        }
+    })
+
+
+    assertNull(authentication)
+    assertNotNull(error)
+    assertTrue(error is OauthErrorResponseException)
+    assertNotNull((error as OauthErrorResponseException).error)
+    assertEquals(INVALID_GRANT.toString(), (error as OauthErrorResponseException).error.errorCode)
+    assertEquals("refresh token expired", (error as OauthErrorResponseException).errorDescription)
+    Mockito.verify(refreshTokenRepository, times(1)).update(refreshToken)
+}
 //
 //    @Test
 //    fun testGetAuthenticationValidToken() {
