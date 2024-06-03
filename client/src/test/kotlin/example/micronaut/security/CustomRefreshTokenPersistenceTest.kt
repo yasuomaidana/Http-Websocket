3716a443f7ab7c804e09db8a012f278b4249fad4
yasuomaidana <yasuomaidana@gmail.com>
2024-06-03 16:55:54 +0900

    Adding testing application test

--- client/src/test/kotlin/example/micronaut/security/CustomRefreshTokenPersistenceTest.kt
+++ client/src/test/kotlin/example/micronaut/security/CustomRefreshTokenPersistenceTest.kt
@@ -6,34 +6,41 @@ import example.micronaut.repository.RefreshTokenRepository
 import io.micronaut.security.authentication.Authentication
 import io.micronaut.security.errors.OauthErrorResponseException
 import io.micronaut.security.token.event.RefreshTokenGeneratedEvent
-import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import io.micronaut.security.errors.IssuingAnAccessTokenErrorCode.INVALID_GRANT
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest
+import jakarta.inject.Inject
 import org.junit.jupiter.api.Assertions.*
 import org.mockito.ArgumentMatchers.any
 import org.mockito.Mock
 import org.mockito.Mockito
 import org.mockito.Mockito.times
 import org.mockito.MockitoAnnotations
+import org.reactivestreams.Subscriber
+import org.reactivestreams.Subscription
 import java.time.Instant
 import java.util.*
 
+@MicronautTest(environments = ["test"])
 class CustomRefreshTokenPersistenceTest {
 
     private lateinit var customRefreshTokenPersistence: CustomRefreshTokenPersistence
 
     @Mock
     private lateinit var refreshTokenRepository: RefreshTokenRepository
-    @Mock
+
+    @Inject
     private lateinit var refreshTokenConfiguration: CustomRefreshTokenConfigurationProperties
 
     @BeforeEach
     fun setUp() {
         MockitoAnnotations.openMocks(this)
         customRefreshTokenPersistence =
-            CustomRefreshTokenPersistence(refreshTokenRepository,
-                refreshTokenConfiguration)
+            CustomRefreshTokenPersistence(
+                refreshTokenRepository,
+                refreshTokenConfiguration
+            )
 
     }
 
@@ -102,18 +109,19 @@ fun testGetAuthenticationValidToken() {
 
     var authentication: Authentication?= null
     var error: Throwable? = null
+
     customRefreshTokenPersistence.getAuthentication("testRefreshToken").subscribe(object : Subscriber<Authentication> {
-        override fun onSubscribe(s: Subscription) {
-            s.request(1)
-        }
+            override fun onSubscribe(s: Subscription) {
+                s.request(1)
+            }
 
-        override fun onNext(t: Authentication) {
-            authentication = t
-        }
+            override fun onNext(t: Authentication) {
+                authentication = t
+            }
 
-        override fun onError(t: Throwable) {
-            error = t
-        }
+            override fun onError(t: Throwable) {
+                error = t
+            }
 
             override fun onComplete() {
             }
@@ -132,8 +140,10 @@ fun testGetAuthenticationNonExistingToken() {
 
     @Test
     fun testPersistToken() {
-        val event = RefreshTokenGeneratedEvent(Authentication.build("testUser"),
-            "testRefreshToken")
+        val event = RefreshTokenGeneratedEvent(
+            Authentication.build("testUser"),
+            "testRefreshToken"
+        )
         customRefreshTokenPersistence.persistToken(event)
 
         Mockito.verify(refreshTokenRepository, times(1)).save(any())
