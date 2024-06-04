1be5c8784454b60620e9ff15322e7084da75df65
yasuomaidana <yasuomaidana@gmail.com>
2024-06-04 19:42:36 +0900

    Changing abstract class to interface for repository

--- client/src/main/kotlin/example/micronaut/repository/BookRepository.kt
+++ client/src/main/kotlin/example/micronaut/repository/BookRepository.kt
@@ -6,6 +6,6 @@ import io.micronaut.data.model.query.builder.sql.Dialect
 import io.micronaut.data.repository.PageableRepository
 
 @JdbcRepository(dialect= Dialect.MYSQL)
-abstract class BookRepository: PageableRepository<Book, Long> {
-    abstract fun findByTitle(title: String): Book?
+interface BookRepository: PageableRepository<Book, Long> {
+    fun findByTitle(title: String): Book?
 }
