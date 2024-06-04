048a150eb05008831486703fdf72710f501c4265
yasuomaidana <yasuomaidana@gmail.com>
2024-06-04 19:42:17 +0900

    Creating fruit model

---
+++ client/src/main/kotlin/example/micronaut/entities/mongo/Fruits.kt
@@ -0,0 +1,18 @@
+package example.micronaut.entities.mongo
+
+import io.micronaut.core.annotation.Introspected
+import io.micronaut.data.annotation.GeneratedValue
+import io.micronaut.data.annotation.Id
+import io.micronaut.data.annotation.MappedEntity
+import org.bson.codecs.pojo.annotations.BsonId
+
+@Introspected
+@MappedEntity
+data class Fruit(
+    @field:Id
+    @field:GeneratedValue
+    @BsonId
+    var id: String? = null,
+    val name:String,
+    val weight:Double,
+)
