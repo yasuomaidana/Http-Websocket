dbb9fb2a9c0e7b59c6ce641b4a2d549fb925010e
yasuomaidana <yasuomaidana@gmail.com>
2024-06-05 20:16:33 +0900

    Creating PostIt model

---
+++ client/src/main/kotlin/example/micronaut/entities/mongo/postit/PostIt.kt
@@ -0,0 +1,18 @@
+package example.micronaut.entities.mongo.postit
+
+import io.micronaut.core.annotation.Introspected
+import io.micronaut.data.annotation.Id
+import io.micronaut.data.annotation.MappedEntity
+import org.bson.types.ObjectId
+
+@Introspected
+@MappedEntity
+data class PostIt (
+    @field:Id
+    var id: ObjectId? = null,
+    val title: String,
+    var content: String,
+    val childPostItIds: List<ObjectId>,
+    var color: String,
+    val commentIds: List<ObjectId>
+)
