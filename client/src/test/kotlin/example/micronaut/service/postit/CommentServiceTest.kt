fd508c457e1d972b5d1639f4e1e0bf6341c78b51
yasuomaidana <yasuomaidana@gmail.com>
2024-06-05 20:14:33 +0900

    Creating comment service tests

--- client/src/test/kotlin/example/micronaut/service/postit/CommentServiceTest.kt
+++ client/src/test/kotlin/example/micronaut/service/postit/CommentServiceTest.kt
@@ -4,6 +4,7 @@ import example.micronaut.entities.mongo.postit.Comment
 import example.micronaut.entities.mongo.postit.Votes
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest
 import jakarta.inject.Inject
+import org.bson.types.ObjectId
 import org.junit.jupiter.api.Test
 
 import org.junit.jupiter.api.Assertions.*
@@ -17,12 +18,54 @@ class CommentServiceTest {
     @Test
     fun createComment() {
         val votes = Votes(likes = 10, dislikes = 2)
-        val comment = Comment(title = "Test comment", description = "Test description", votes = votes)
-        val result = commentService.createComment(comment).block()
+        val comment = Comment(postId = ObjectId(), title = "Test comment", content = "Test description", votes = votes)
+        val result = commentService.createComment(comment).block()!!
+
         assertNotNull(result)
-        assertEquals(comment.title, result?.title)
-        assertEquals(comment.description, result?.description)
-        assertEquals(comment.votes.likes, result?.votes?.likes)
-        assertEquals(comment.votes.dislikes, result?.votes?.dislikes)
+        assertEquals(comment.postId, result.postId)
+        assertEquals(comment.title, result.title)
+        assertEquals(comment.content, result.content)
+        assertEquals(comment.votes.likes, result.votes.likes)
+        assertEquals(comment.votes.dislikes, result.votes.dislikes)
+    }
+
+    @Test
+    fun getComment() {
+        val votes = Votes(likes = 10, dislikes = 2)
+        val comment = Comment(postId = ObjectId(), title = "Title", content = "Test comment", votes = votes)
+        val createdComment = commentService.createComment(comment).block()
+        val result = commentService.getComment(createdComment!!.id!!)
+        assertTrue(result.isPresent)
+        assertEquals(createdComment.id, result.get().id)
+        assertEquals(createdComment.postId, result.get().postId)
+        assertEquals(createdComment.content, result.get().content)
+        assertEquals(createdComment.votes.likes, result.get().votes.likes)
+        assertEquals(createdComment.votes.dislikes, result.get().votes.dislikes)
+    }
+
+    @Test
+    fun deleteComment() {
+        val votes = Votes(likes = 10, dislikes = 2)
+        val comment = Comment(postId = ObjectId(), title = "Title", content = "Test comment", votes = votes)
+        val createdComment = commentService.createComment(comment).block()
+        val deletedCount = commentService.deleteComment(createdComment!!.id!!).block()!!
+        assertEquals(1, deletedCount)
+        val result = commentService.getComment(createdComment.id!!)
+        assertFalse(result.isPresent)
+    }
+
+    @Test
+    fun updateComment() {
+        val votes = Votes(likes = 10, dislikes = 2)
+        val comment = Comment(postId = ObjectId(), title = "Test comment", votes = votes)
+        val createdComment = commentService.createComment(comment).block()
+        val updatedComment = createdComment!!.copy(title = "Updated comment")
+        val result = commentService.updateComment(updatedComment).block()!!
+        assertNotNull(result)
+        assertEquals(updatedComment.id, result.id)
+        assertEquals(updatedComment.postId, result.postId)
+        assertEquals(updatedComment.title, result.title)
+        assertEquals(updatedComment.votes.likes, result.votes.likes)
+        assertEquals(updatedComment.votes.dislikes, result.votes.dislikes)
     }
 }
