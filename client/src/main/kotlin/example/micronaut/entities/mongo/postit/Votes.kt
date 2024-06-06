fd02ab882bf4e04ec4439b63de23018ca3c5362f
yasuomaidana <yasuomaidana@gmail.com>
2024-06-06 19:28:17 +0900

    Removing unused import

--- client/src/main/kotlin/example/micronaut/entities/mongo/postit/Votes.kt
+++ client/src/main/kotlin/example/micronaut/entities/mongo/postit/Votes.kt
@@ -2,7 +2,7 @@ package example.micronaut.entities.mongo.postit
 
 import io.micronaut.core.annotation.Introspected
 import io.micronaut.data.annotation.Id
-import io.micronaut.data.annotation.MappedEntity
+//import io.micronaut.data.annotation.MappedEntity
 import io.micronaut.serde.annotation.Serdeable
 import org.bson.types.ObjectId
 
