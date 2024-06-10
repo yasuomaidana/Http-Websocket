465402e5474f24048e16e59b2e069db7cf736679
yasuomaidana <yasuomaidana@gmail.com>
2024-06-10 17:49:31 +0900

    Creating constructor to reduce verbosity

--- client/src/main/kotlin/example/micronaut/entities/mongo/postit/Comment.kt
+++ client/src/main/kotlin/example/micronaut/entities/mongo/postit/Comment.kt
@@ -14,4 +14,10 @@ data class Comment(
     val title: String,
     var content: String?= null,
     val votes: Votes = Votes(likes = 0, dislikes = 0)
-)
+){
+    constructor(
+        postId: String,
+        title: String,
+        content: String?
+    ): this(null, ObjectId(postId), title, content)
+}
