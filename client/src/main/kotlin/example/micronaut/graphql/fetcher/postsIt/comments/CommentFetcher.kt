18ba3966b906b95d4c60d22c2a53a5dd712c09eb
yasuomaidana <yasuomaidana@gmail.com>
2024-06-10 19:53:48 +0900

    Changing service to manager

--- client/src/main/kotlin/example/micronaut/graphql/fetcher/postsIt/comments/CommentFetcher.kt
+++ client/src/main/kotlin/example/micronaut/graphql/fetcher/postsIt/comments/CommentFetcher.kt
@@ -1,6 +1,7 @@
 package example.micronaut.graphql.fetcher.postsIt.comments
 
 import example.micronaut.entities.mongo.postit.Comment
+import example.micronaut.manager.PostItManager
 import example.micronaut.service.postit.CommentService
 import graphql.schema.DataFetcher
 import graphql.schema.DataFetchingEnvironment
@@ -8,10 +9,10 @@ import jakarta.inject.Singleton
 
 @Singleton
 class CommentFetcher(
-    private val commentService: CommentService
+    private val postItManager: PostItManager
 ): DataFetcher<Comment> {
     override fun get(environment: DataFetchingEnvironment?): Comment? {
         val id = environment?.getArgument<String>("id")!!
-        return commentService.getComment(id).toFuture().get()
+        return postItManager.getComment(id).toFuture().get()
     }
 }
