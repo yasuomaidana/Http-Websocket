5607f5c4a346a1d302e4a6aeb6163356bcb420bd
yasuomaidana <yasuomaidana@gmail.com>
2024-06-10 15:54:48 +0900

    Removing unused repository

--- client/src/main/kotlin/example/micronaut/repository/postit/comments/CommentGetRepository.kt
+++
@@ -1,9 +0,0 @@
-package example.micronaut.repository.postit.comments
-
-import example.micronaut.configuration.mongo.DefaultMongoRepository
-import example.micronaut.entities.mongo.postit.Comment
-import io.micronaut.data.repository.CrudRepository
-import org.bson.types.ObjectId
-
-@DefaultMongoRepository
-interface CommentGetRepository: CrudRepository<Comment, ObjectId>
