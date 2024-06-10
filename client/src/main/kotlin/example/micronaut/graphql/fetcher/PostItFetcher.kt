5117655fe6c884d958c8f8c5217f0070624e411b
yasuomaidana <yasuomaidana@gmail.com>
2024-06-10 16:43:05 +0900

    Removing unused imports

--- client/src/main/kotlin/example/micronaut/graphql/fetcher/PostItFetcher.kt
+++ client/src/main/kotlin/example/micronaut/graphql/fetcher/PostItFetcher.kt
@@ -1,9 +1,7 @@
 package example.micronaut.graphql.fetcher
 
-import example.micronaut.entities.mongo.postit.Comment
 import example.micronaut.entities.mongo.postit.PostIt
 import example.micronaut.manager.PostItManager
-import example.micronaut.repository.postit.PostItRepository
 import graphql.schema.DataFetcher
 import graphql.schema.DataFetchingEnvironment
 import jakarta.inject.Singleton
@@ -11,7 +9,6 @@ import jakarta.inject.Singleton
 @Singleton
 class PostItFetcher(
     private val postItManager: PostItManager,
-    private val postItRepository: PostItRepository
     ) : DataFetcher<PostIt> {
     override fun get(env: DataFetchingEnvironment): PostIt? {
         // Use the postItManager to add a comment to a Post-it
