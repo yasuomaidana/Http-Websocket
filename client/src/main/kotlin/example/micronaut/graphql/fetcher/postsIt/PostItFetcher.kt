81cc0e570886b8449346382d74eae298d4174981
yasuomaidana <yasuomaidana@gmail.com>
2024-06-11 14:44:15 +0900

    Changing Returning type

--- client/src/main/kotlin/example/micronaut/graphql/fetcher/postsIt/PostItFetcher.kt
+++ client/src/main/kotlin/example/micronaut/graphql/fetcher/postsIt/PostItFetcher.kt
@@ -1,6 +1,8 @@
 package example.micronaut.graphql.fetcher.postsIt
 
 import example.micronaut.entities.mongo.postit.PostIt
+import example.micronaut.graphql.dto.PostItDTO
+import example.micronaut.graphql.mapper.PostItMapper
 import example.micronaut.manager.PostItManager
 import graphql.schema.DataFetcher
 import graphql.schema.DataFetchingEnvironment
@@ -9,10 +11,11 @@ import jakarta.inject.Singleton
 @Singleton
 class PostItFetcher(
     private val postItManager: PostItManager,
-    ) : DataFetcher<PostIt> {
-    override fun get(env: DataFetchingEnvironment): PostIt? {
+    private val postItMapper: PostItMapper
+    ) : DataFetcher<PostItDTO> {
+    override fun get(env: DataFetchingEnvironment): PostItDTO {
         // Use the postItManager to add a comment to a Post-it
         val postItId = env.getArgument<String>("id")!!
-        return postItManager.getPostIt(postItId).toFuture().get()
+        return postItMapper.toPostItDTO(postItManager.getPostIt(postItId).toFuture().get())
     }
 }
--- client/src/main/kotlin/example/micronaut/graphql/fetcher/postsIt/PostsFetcher.kt
+++ client/src/main/kotlin/example/micronaut/graphql/fetcher/postsIt/PostsFetcher.kt
@@ -1,6 +1,8 @@
 package example.micronaut.graphql.fetcher.postsIt
 
 import example.micronaut.entities.mongo.postit.PostIt
+import example.micronaut.graphql.dto.PostItDTO
+import example.micronaut.graphql.mapper.PostItMapper
 import example.micronaut.manager.PostItManager
 import graphql.schema.DataFetcher
 import graphql.schema.DataFetchingEnvironment
@@ -8,9 +10,11 @@ import jakarta.inject.Singleton
 
 @Singleton
 class PostsFetcher(
-    private val postItManager: PostItManager
-) : DataFetcher<List<PostIt>> {
-    override fun get(environment: DataFetchingEnvironment?): List<PostIt> {
+    private val postItManager: PostItManager,
+    private val postItMapper: PostItMapper
+) : DataFetcher<List<PostItDTO>> {
+    override fun get(environment: DataFetchingEnvironment?): List<PostItDTO> {
         return postItManager.getPosts().collectList().toFuture().get()
+            .map {postItMapper.toPostItDTO(it)  }
     }
 }
