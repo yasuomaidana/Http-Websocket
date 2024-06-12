eb759f2d651005824fdffe848cbd5c2a71a80d16
yasuomaidana <yasuomaidana@gmail.com>
2024-06-12 15:12:56 +0900

    Refactoring post mapper

--- client/src/main/kotlin/example/micronaut/graphql/mapper/PostItMapper.kt
+++ client/src/main/kotlin/example/micronaut/graphql/mapper/PostItMapper.kt
@@ -1,13 +1,12 @@
 package example.micronaut.graphql.mapper
 
-import example.micronaut.entities.mongo.postit.Comment
 import example.micronaut.entities.mongo.postit.PostIt
+import example.micronaut.graphql.dto.CommentPageDTO
 import example.micronaut.graphql.dto.PostItDTO
 import example.micronaut.graphql.dto.PostItPageDTO
 import example.micronaut.manager.PostItManager
 import io.micronaut.data.model.Page
 import jakarta.inject.Inject
-import org.mapstruct.Named
 import org.bson.types.ObjectId
 import org.mapstruct.Mapper
 import org.mapstruct.Mapping
@@ -20,45 +19,43 @@ abstract class PostItMapper {
     @Inject
     lateinit var postItManager: PostItManager
 
+    @Inject
+    lateinit var commentMapper: CommentMapper
+
     @Mappings(
-        Mapping(target = "childPosts", expression = "java(postItIdToPostIt(childPosts, kids))"),
-        Mapping(target = "comments", source = "postId.commentIds", qualifiedByName = ["commentIdsToComments"]),
-        Mapping(target = "totalPages", expression = "java(childPosts.getTotalPages())"),
-        Mapping(target = "currentPage", expression = "java(childPosts.getPageNumber())"),
+        Mapping(target = "childPosts", expression = "java(postItPageToDTO(childPosts, kids))"),
+        Mapping(target = "comments", expression="java(commentIdsToCommentsPage(postId.getCommentIds(), kids))"),
         Mapping(target = "id", source = "postId.id"),
         Mapping(target = "content", source = "postId.content"),
-        Mapping(target="totalChildPosts", expression="java(childPosts.getTotalSize())")
     )
-    abstract fun toPostItDTO(postId: PostIt, childPosts: Page<PostIt>,kids:Int): PostItDTO
+    abstract fun postItToPostItDTO(postId: PostIt, childPosts: Page<PostIt>,kids:Int): PostItDTO
 
-    fun toPostItDTO(postIt:PostIt, kids:Int=10, offset:Int=0): PostItDTO {
+    fun postItToPostItDTO(postIt:PostIt, kids:Int=10, offset:Int=0): PostItDTO {
         val postsPages = postItManager.getPostsByIds(postIt.childPostItIds, offset, kids).toFuture().get()
 
-        return toPostItDTO(postIt, postsPages,kids)
+        return postItToPostItDTO(postIt, postsPages,kids)
     }
 
     @Mappings(
-        Mapping(target = "content", expression = "java(postItIdToPostIt(postsPage, limit))"),
+        Mapping(target = "content", expression = "java(postItPageToListDTO(postsPage, postsLimit))"),
         Mapping(target = "totalPages", expression = "java(postsPage.getTotalPages())"),
         Mapping(target = "currentPage", expression = "java(postsPage.getPageNumber())"),
         Mapping(target = "totalPosts", expression = "java(postsPage.getTotalSize())")
     )
-    abstract fun toPostItPageDTO(postsPage:Page<PostIt>, limit: Int): PostItPageDTO
+    abstract fun postItPageToDTO(postsPage:Page<PostIt>, postsLimit: Int): PostItPageDTO
 
-    fun postItIdToPostIt(childPosts: Page<PostIt>, kids: Int): List<PostItDTO> {
+    fun postItPageToListDTO(childPosts: Page<PostIt>, kids: Int): List<PostItDTO> {
         return Flux.fromIterable(childPosts)
-            .map { toPostItDTO(it, kids) }
+            .map { postItToPostItDTO(it, kids, 0) }
             .collectList()
             .toFuture()
             .get()
     }
 
-    @Named("commentIdsToComments")
-    fun commentIdsToComments(commentIds: List<ObjectId>): List<Comment> {
-        return Flux.fromIterable(commentIds)
-            .flatMap { postItManager.getComment(it) }
-            .collectList()
-            .toFuture()
-            .get()
-    }
+    fun commentIdsToCommentsPage(commentIds: List<ObjectId>, commentLimit:Int): CommentPageDTO
+    =
+        postItManager.getComments(commentIds, 0, commentLimit).toFuture().get().let {
+             commentMapper.commentPageToCommentPageDTO(it)
+        }
+
 }
--- client/src/test/kotlin/example/micronaut/graphql/mapper/PostItMapperTest.kt
+++ client/src/test/kotlin/example/micronaut/graphql/mapper/PostItMapperTest.kt
@@ -2,6 +2,7 @@ package example.micronaut.graphql.mapper
 
 import example.micronaut.entities.mongo.postit.Comment
 import example.micronaut.entities.mongo.postit.PostIt
+import example.micronaut.graphql.dto.CommentPageDTO
 import example.micronaut.manager.PostItManager
 import io.micronaut.data.model.Page
 import io.micronaut.data.model.Pageable
@@ -9,7 +10,7 @@ import org.bson.types.ObjectId
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.Assertions.*
 import org.mockito.*
-import org.mockito.ArgumentMatchers.any
+import org.mockito.ArgumentMatchers.*
 import reactor.core.publisher.Mono
 
 
@@ -17,6 +18,8 @@ class PostItMapperTest {
     @InjectMocks
     private lateinit var postItMapper: PostItMapperImpl
 
+    @Mock
+    private lateinit var commentMapper: CommentMapperImpl
     @Mock
     private lateinit var postItManager: PostItManager
 
@@ -51,24 +54,24 @@ class PostItMapperTest {
     @Test
     fun `test toPostItDTO with valid post-it`() {
 
-        Mockito.`when`(postItManager.getPostIt(
-            any(ObjectId::class.java)?: ObjectId()))
-            .thenReturn(Mono.just(returnedPostIt))
+        val postItPage = Page.of(listOf(returnedPostIt, returnedPostIt), Pageable.from(0), 2)
+        Mockito.`when`(postItManager.getPostsByIds(postIt.childPostItIds, 0, 10))
+            .thenReturn(Mono.just(postItPage))
 
-        Mockito.`when`(postItManager.getComment(any(ObjectId::class.java)?: ObjectId()))
-            .thenReturn(Mono.just(returnedComment))
-
-        Mockito.`when`(postItManager.getPostsByIds(postIt.childPostItIds,
-            0, 10))
-            .thenReturn(Mono.just(Page.of(listOf(returnedPostIt,returnedPostIt), Pageable.from(0), 2)))
-
-        Mockito.`when`(postItManager.getPostsByIds( emptyList(),
-            0, 10))
+        Mockito.`when`(postItManager.getPostsByIds(emptyList(), 0, 10))
             .thenReturn(Mono.just(Page.of(emptyList(), Pageable.from(0), 0)))
 
-        val postItDTO = postItMapper.toPostItDTO(postIt)
-        assertNotNull(postItDTO)
-        assertEquals(2, postItDTO.childPosts.size)
-        assertEquals(2, postItDTO.comments.size)
+        Mockito.`when`(postItManager.getComments(anyList<ObjectId>(), anyInt(),anyInt()))
+            .thenReturn(Mono.just(Page.of(listOf(returnedComment), Pageable.from(0), 1)))
+
+        Mockito.`when`(commentMapper.commentPageToCommentPageDTO(any<Page<Comment>>()))
+            .thenReturn(CommentPageDTO(listOf(returnedComment), 1, 0, 1))
+
+        val postItPageDTO = postItMapper.postItPageToDTO(postItPage, 10)
+        assertNotNull(postItPageDTO)
+        assertEquals(2, postItPageDTO.content.size)
+        assertEquals(1, postItPageDTO.totalPages)
+        assertEquals(0, postItPageDTO.currentPage)
+        assertEquals(2, postItPageDTO.totalPosts)
     }
 }
