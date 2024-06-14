b3f317bbd8db45ec570ac51f9eca48563c886870
yasuomaidana <yasuomaidana@gmail.com>
2024-06-14 14:34:10 +0900

    Organizing error handling on GraphQL

--- client/src/main/kotlin/example/micronaut/graphql/handler/CustomDataFetcherExceptionHandler.kt
+++ client/src/main/kotlin/example/micronaut/graphql/handler/CustomDataFetcherExceptionHandler.kt
@@ -1,6 +1,9 @@
 package example.micronaut.graphql.handler
 
 import example.micronaut.exception.ForbiddenException
+import example.micronaut.exception.NotFoundException
+import example.micronaut.exception.NullIdException
+import example.micronaut.exception.UserNotAuthenticatedException
 import graphql.GraphqlErrorBuilder
 import graphql.execution.DataFetcherExceptionHandler
 import graphql.execution.DataFetcherExceptionHandlerParameters
@@ -13,17 +16,22 @@ import java.util.concurrent.CompletableFuture
 @Singleton
 class CustomDataFetcherExceptionHandler:DataFetcherExceptionHandler {
     private val logger: Logger = LoggerFactory.getLogger(CustomDataFetcherExceptionHandler::class.java)
-    override fun handleException(handlerParameters: DataFetcherExceptionHandlerParameters?): CompletableFuture<DataFetcherExceptionHandlerResult> {
-        if (handlerParameters?.exception is ForbiddenException) {
-            val exception = handlerParameters.exception as ForbiddenException
+    override fun handleException(handlerParameters: DataFetcherExceptionHandlerParameters): CompletableFuture<DataFetcherExceptionHandlerResult> {
+        return buildErrorMessage(handlerParameters.exception as Exception) ?: throw handlerParameters.exception
+    }
+
+    private fun buildErrorMessage(exception: Exception): CompletableFuture<DataFetcherExceptionHandlerResult>? {
+        val supportedErrors = listOf(ForbiddenException::class.java,
+            NotFoundException::class.java, NullIdException::class.java,
+            UserNotAuthenticatedException::class.java)
+        if (supportedErrors.contains(exception::class.java)) {
             logger.error(exception.message)
             val error = GraphqlErrorBuilder.newError()
                 .message(exception.message)
                 .build()
-//            return CompletableFuture.failedFuture(exception)
             return CompletableFuture.completedFuture(DataFetcherExceptionHandlerResult.newResult().error(error).build())
         } else {
-            throw handlerParameters?.exception!! // rethrow the exception if it's not an UnauthorizedException or custom supported error
+            return null
         }
     }
 }
