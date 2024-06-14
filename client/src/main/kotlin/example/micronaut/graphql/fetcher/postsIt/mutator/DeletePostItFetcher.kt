da8fd81ea598def8fcc6c8ac90b53bca8696970c
yasuomaidana <yasuomaidana@gmail.com>
2024-06-14 14:34:41 +0900

    Changing boolean to float and using custom errors

--- client/src/main/kotlin/example/micronaut/graphql/fetcher/postsIt/mutator/DeletePostItFetcher.kt
+++ client/src/main/kotlin/example/micronaut/graphql/fetcher/postsIt/mutator/DeletePostItFetcher.kt
@@ -2,6 +2,9 @@ package example.micronaut.graphql.fetcher.postsIt.mutator
 
 import example.micronaut.entities.user.ADMIN_ROLE
 import example.micronaut.exception.ForbiddenException
+import example.micronaut.exception.NotFoundException
+import example.micronaut.exception.NullIdException
+import example.micronaut.exception.UserNotAuthenticatedException
 import example.micronaut.manager.PostItManager
 import graphql.schema.DataFetcher
 import graphql.schema.DataFetchingEnvironment
@@ -13,18 +16,18 @@ import jakarta.inject.Singleton
 class DeletePostItFetcher(
     private val postItManager: PostItManager,
     @Inject private val securityService: SecurityService,
-): DataFetcher<Boolean> {
+): DataFetcher<Long> {
 
-    override fun get(env: DataFetchingEnvironment?): Boolean {
-        val id = env?.getArgument<String>("id") ?: throw IllegalArgumentException("ID cannot be null")
+    override fun get(env: DataFetchingEnvironment?): Long {
+        val id = env?.getArgument<String>("id") ?: throw NullIdException()
 
-        val username = securityService.username().orElseGet { throw IllegalArgumentException("User not authenticated") }
-        val postIt = postItManager.getPostIt(id).toFuture().get()  ?: throw IllegalArgumentException("Post-it not found")
+        val username = securityService.username().orElseGet { throw UserNotAuthenticatedException()}
+        val postIt = postItManager.getPostIt(id).toFuture().get()  ?: throw NotFoundException(type = "PostIt",id=id)
 
         if (postIt.createdBy != username && !securityService.hasRole(ADMIN_ROLE)) {
             throw ForbiddenException(user=username, action="delete post-it: $id")
         }
-        return postItManager.deletePostIt(id).toFuture().isDone
+        return postItManager.deletePostIt(id).toFuture().get()
     }
 
 }
