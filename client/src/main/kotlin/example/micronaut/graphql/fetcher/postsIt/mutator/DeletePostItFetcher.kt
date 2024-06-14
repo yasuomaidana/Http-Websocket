a4cb63d43595d5c3d08f6abc8221351e9ed66922
yasuomaidana <yasuomaidana@gmail.com>
2024-06-14 14:36:36 +0900

    Adding missing imports

--- client/src/main/kotlin/example/micronaut/graphql/fetcher/postsIt/mutator/DeletePostItFetcher.kt
+++ client/src/main/kotlin/example/micronaut/graphql/fetcher/postsIt/mutator/DeletePostItFetcher.kt
@@ -2,6 +2,9 @@ package example.micronaut.graphql.fetcher.postsIt.mutator
 
 import example.micronaut.entities.user.ADMIN_ROLE
 import example.micronaut.exception.ForbiddenException
+import example.micronaut.exception.NotFoundException
+import example.micronaut.exception.NullIdException
+import example.micronaut.exception.UserNotAuthenticatedException
 import example.micronaut.manager.PostItManager
 import graphql.schema.DataFetcher
 import graphql.schema.DataFetchingEnvironment
@@ -18,7 +21,7 @@ class DeletePostItFetcher(
     override fun get(env: DataFetchingEnvironment?): Long {
         val id = env?.getArgument<String>("id") ?: throw NullIdException()
 
-        val username = securityService.username().orElseGet { throw UserNotAuthenticatedException()}
+        val username = securityService.username().orElseGet { throw UserNotAuthenticatedException() }
         val postIt = postItManager.getPostIt(id).toFuture().get()  ?: throw NotFoundException(type = "PostIt",id=id)
 
         if (postIt.createdBy != username && !securityService.hasRole(ADMIN_ROLE)) {
