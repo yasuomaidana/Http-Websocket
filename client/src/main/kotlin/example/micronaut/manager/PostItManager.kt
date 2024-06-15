a6d627226015ac63c5cd4336f06f58a4563d436f
yasuomaidana <yasuomaidana@gmail.com>
2024-06-15 16:48:32 +0900

    Registering comments and post its creation and deletion to user activity.

--- client/src/main/kotlin/example/micronaut/manager/PostItManager.kt
+++ client/src/main/kotlin/example/micronaut/manager/PostItManager.kt
@@ -2,6 +2,7 @@ package example.micronaut.manager
 
 import example.micronaut.entities.mongo.postit.Comment
 import example.micronaut.entities.mongo.postit.PostIt
+import example.micronaut.service.UserActivityService
 import example.micronaut.service.postit.CommentService
 import example.micronaut.service.postit.PostItService
 import jakarta.inject.Singleton
@@ -13,13 +14,22 @@ import reactor.core.publisher.Mono
 @Singleton
 class PostItManager(
     private val postItService: PostItService,
-    private val commentService: CommentService
+    private val commentService: CommentService,
+    private val userActivityService: UserActivityService,
 ) {
     private val logger: Logger = LoggerFactory.getLogger(PostItManager::class.java)
 
-    fun createPostIt(postIt: PostIt): Mono<PostIt> {
-        return postItService.createPostIt(postIt)
-    }
+    fun createPostIt(postIt: PostIt): Mono<PostIt> =
+        postItService.createPostIt(postIt)
+            .flatMap { createdPostIt ->
+                userActivityService.getUserActivity(createdPostIt.createdBy)
+                    .flatMap { userActivity ->
+                        userActivity.postItIds += createdPostIt.id!!
+                        userActivityService.updateUserActivity(userActivity)
+                    }
+                    .then(Mono.just(createdPostIt))
+            }
+
 
     fun addCommentToPostIt(postItId: String, comment: Comment): Mono<PostIt> {
         return addCommentToPostIt(ObjectId(postItId), comment)
@@ -27,8 +37,11 @@ class PostItManager(
 
     fun addCommentToPostIt(postItId: ObjectId, comment: Comment): Mono<PostIt> =
         commentService.createComment(comment).flatMap { createdComment ->
-                postItService.addCommentToPostIt(postItId, createdComment.id!!)
-            }
+            userActivityService.getUserActivity(createdComment.createdBy).flatMap { userActivity ->
+                userActivity.commentIds += createdComment.id!!
+                userActivityService.updateUserActivity(userActivity)
+            }.then(postItService.addCommentToPostIt(postItId, createdComment.id!!))
+        }
 
     fun removeCommentFromPostIt(postItId: ObjectId, commentId: ObjectId): Mono<PostIt> =
         commentService.assertExits(commentId).flatMap {
@@ -38,24 +51,42 @@ class PostItManager(
     fun getPostIt(id: ObjectId) = postItService.getPostIt(id)
 
 
-    fun getPostIt(id:String) = getPostIt(ObjectId(id))
+    fun getPostIt(id: String) = getPostIt(ObjectId(id))
 
     fun getPosts(offset: Int, limit: Int) = postItService.getPosts(offset, limit)
 
     fun getComment(id: String) = getComment(ObjectId(id))
     fun getComment(id: ObjectId) = commentService.getComment(id)
 
-    fun createChildPostIt(parentId: ObjectId, childPostIt: PostIt) = postItService.createChildPostIt(parentId, childPostIt)
+    fun createChildPostIt(parentId: ObjectId, childPostIt: PostIt) =
+        postItService.createChildPostIt(parentId, childPostIt).flatMap { createdPostIt ->
+            userActivityService.getUserActivity(createdPostIt.createdBy)
+                .flatMap { userActivity ->
+                    userActivity.postItIds += createdPostIt.id!!
+                    userActivityService.updateUserActivity(userActivity)
+                }
+                .then(Mono.just(createdPostIt))
+        }
+
     fun createChildPostIt(postItId: String, childPostIt: PostIt) = createChildPostIt(ObjectId(postItId), childPostIt)
 
-    fun addChildPostIt(postItId: ObjectId, childPostItId: ObjectId) = postItService.addChildPostIt(postItId, childPostItId)
-    fun addChildPostIt(postItId: String, childPostItId: String) = addChildPostIt(ObjectId(postItId), ObjectId(childPostItId))
+    fun addChildPostIt(postItId: ObjectId, childPostItId: ObjectId) =
+        postItService.addChildPostIt(postItId, childPostItId)
 
-    fun removeChildPostIt(postItId: ObjectId, childPostItId: ObjectId) = postItService.removeChildPostIt(postItId, childPostItId)
-    fun removeChildPostIt(postItId: String, childPostItId: String) = removeChildPostIt(ObjectId(postItId), ObjectId(childPostItId))
+    fun addChildPostIt(postItId: String, childPostItId: String) =
+        addChildPostIt(ObjectId(postItId), ObjectId(childPostItId))
 
-    fun changeParentPostIt(parentId:ObjectId, childPostItId: ObjectId, newParentPostItId: ObjectId) = postItService.changeParentPostIt(parentId,childPostItId, newParentPostItId)
-    fun changeParentPostIt(parentId:String, childPostItId: String, newParentPostItId: String) = changeParentPostIt(ObjectId(parentId), ObjectId(childPostItId), ObjectId(newParentPostItId))
+    fun removeChildPostIt(postItId: ObjectId, childPostItId: ObjectId) =
+        postItService.removeChildPostIt(postItId, childPostItId)
+
+    fun removeChildPostIt(postItId: String, childPostItId: String) =
+        removeChildPostIt(ObjectId(postItId), ObjectId(childPostItId))
+
+    fun changeParentPostIt(parentId: ObjectId, childPostItId: ObjectId, newParentPostItId: ObjectId) =
+        postItService.changeParentPostIt(parentId, childPostItId, newParentPostItId)
+
+    fun changeParentPostIt(parentId: String, childPostItId: String, newParentPostItId: String) =
+        changeParentPostIt(ObjectId(parentId), ObjectId(childPostItId), ObjectId(newParentPostItId))
 
     fun getPostsByIds(ids: List<ObjectId>, offset: Int, limit: Int) = postItService.getByIds(ids, offset, limit)
 
@@ -64,18 +95,32 @@ class PostItManager(
     fun getCommentFromPostIt(postItId: ObjectId, offset: Int, limit: Int) = getPostIt(postItId).flatMap { postIt ->
         getComments(postIt.commentIds, offset, limit)
     }
-    fun getCommentFromPostIt(postItId: String, offset: Int, limit: Int) = getCommentFromPostIt(ObjectId(postItId), offset, limit)
 
-    fun deletePostIt(id: ObjectId) = postItService.deletePostIt(id)
+    fun getCommentFromPostIt(postItId: String, offset: Int, limit: Int) =
+        getCommentFromPostIt(ObjectId(postItId), offset, limit)
+
+    fun deletePostIt(id: ObjectId) =
+        postItService.getPostIt(id).flatMap { postIt ->
+            userActivityService.getUserActivity(postIt.createdBy).flatMap { userActivity ->
+                userActivity.postItIds -= postIt.id!!
+                userActivityService.updateUserActivity(userActivity)
+            }.then(postItService.deletePostIt(id))
+        }
+
     fun deletePostIt(id: String) = deletePostIt(ObjectId(id))
 
-    fun deleteComment(id: ObjectId):Mono<Long> =
+    fun deleteComment(id: ObjectId): Mono<Long> =
         commentService.getComment(id).flatMap { comment ->
-            postItService.removeCommentFromPostIt(comment.postId, comment.id!!)
-                .onErrorResume { error ->
-                    logger.warn("Error removing comment from post-it", error)
-                    Mono.empty()}
-                .then(commentService.deleteComment(id))
+            userActivityService.getUserActivity(comment.createdBy).flatMap { userActivity ->
+                userActivity.commentIds -= comment.id!!
+                userActivityService.updateUserActivity(userActivity)
+            }.then(
+                postItService.removeCommentFromPostIt(comment.postId, comment.id!!)
+                    .onErrorResume { error ->
+                        logger.warn("Error removing comment from post-it", error)
+                        Mono.empty()
+                    }
+            ).then(commentService.deleteComment(id))
         }
 
     fun deleteComment(id: String) = deleteComment(ObjectId(id))
@@ -95,15 +140,14 @@ class PostItManager(
             commentService.updateComment(comment)
         }
 
-    fun changeCommentPostIt(commentId: ObjectId, newPostItId: ObjectId): Mono<Comment>
-    = postItService.assertExists(newPostItId).then(
-        commentService.getComment(commentId).flatMap { comment ->
-            postItService.removeCommentFromPostIt(comment.postId, commentId)
-                .flatMap { postItService.addCommentToPostIt(newPostItId, commentId)}
-                .flatMap { comment.postId = newPostItId; commentService.updateComment(comment)}
-        }
-    )
-
+    fun changeCommentPostIt(commentId: ObjectId, newPostItId: ObjectId): Mono<Comment> =
+        postItService.assertExists(newPostItId).then(
+            commentService.getComment(commentId).flatMap { comment ->
+                postItService.removeCommentFromPostIt(comment.postId, commentId)
+                    .flatMap { postItService.addCommentToPostIt(newPostItId, commentId) }
+                    .flatMap { comment.postId = newPostItId; commentService.updateComment(comment) }
+            }
+        )
 
     fun changeCommentPostIt(commentId: String, newPostItId: String) =
         changeCommentPostIt(ObjectId(commentId), ObjectId(newPostItId))
