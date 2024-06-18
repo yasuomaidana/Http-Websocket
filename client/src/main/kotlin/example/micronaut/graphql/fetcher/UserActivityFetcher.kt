05021a72d553709c721dde2571cf60da57c1074e
yasuomaidana <yasuomaidana@gmail.com>
2024-06-18 18:00:41 +0900

    Setting default username function

--- client/src/main/kotlin/example/micronaut/graphql/fetcher/UserActivityFetcher.kt
+++ client/src/main/kotlin/example/micronaut/graphql/fetcher/UserActivityFetcher.kt
@@ -1,5 +1,8 @@
 package example.micronaut.graphql.fetcher
 
+import example.micronaut.entities.user.ADMIN_ROLE
+import example.micronaut.exception.ForbiddenException
+import example.micronaut.exception.UserNotAuthenticatedException
 import example.micronaut.graphql.dto.UserActivityDTO
 import example.micronaut.graphql.mapper.CommentMapper
 import example.micronaut.graphql.mapper.PostItMapper
@@ -7,6 +10,8 @@ import example.micronaut.manager.PostItManager
 import example.micronaut.service.UserActivityService
 import graphql.schema.DataFetcher
 import graphql.schema.DataFetchingEnvironment
+import io.micronaut.security.utils.SecurityService
+import jakarta.inject.Inject
 import jakarta.inject.Singleton
 
 @Singleton
@@ -14,15 +19,22 @@ class UserActivityFetcher(
     private val userActivityService: UserActivityService,
     private val postItManager: PostItManager,
     private val postItMapper: PostItMapper,
-    private val commentMapper: CommentMapper
+    private val commentMapper: CommentMapper,
+    @Inject
+    private val securityService: SecurityService
 ):DataFetcher<UserActivityDTO> {
-    override fun get(environment: DataFetchingEnvironment?): UserActivityDTO {
-        val username = environment?.getArgument<String>("username")!!
+    override fun get(environment: DataFetchingEnvironment): UserActivityDTO {
+        val username = environment.getArgument<String>("username") ?: securityService.username().orElseGet { throw UserNotAuthenticatedException() }
+
         val postsOffset = environment.getArgument<Int>("postsOffset") ?: 0
         val postsLimit = environment.getArgument<Int>("postsLimit") ?: 10
         val commentsOffset = environment.getArgument<Int>("commentsOffset") ?: 0
         val commentsLimit = environment.getArgument<Int>("commentsLimit") ?: 10
         val userActivity = userActivityService.getUserActivity(username).toFuture().get()
+
+        if (userActivity.username != securityService.username().get() && !securityService.hasRole(ADMIN_ROLE)) {
+            throw ForbiddenException(user=username, action="view user activity: $username")
+        }
 
         val postsPage = postItManager.getPostsByIds(userActivity.postItIds, postsOffset, postsLimit).toFuture().get()
         val postIts = postItMapper.postItPageToDTO(postsPage, postsLimit)
